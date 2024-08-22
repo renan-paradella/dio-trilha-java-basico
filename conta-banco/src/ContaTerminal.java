7a372dc4592ece379715105caeda7c83a750d1d0
renan-paradella <renanparadella@hotmail.com>
2024-08-22 08:28:34 -0300

    início para testes

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        
+        //TODO: Conhecer e importar a classe Scanner
+
+        //Exibir as mensagens para nosso usuário
+
+        //Obter pela classe Scanner os valores digitados no terminal
+
+        //Exibir a mensagem final da conta criada
+
+        int numero = 0001;
+        String agencia = "1234-5";
+        String nome_cliente = "";
+        float saldo = 0;
+
+        System.out.println("Por favor, digite o número da agência!");
+
+
+
+        System.out.println("Olá " + nome_cliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
+
     }
 }
