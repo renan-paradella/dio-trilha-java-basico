5ea40fdefbceb36f46e9fc4cbdcdeaef0232fd1b
renan-paradella <renanparadella@hotmail.com>
2024-08-22 22:00:24 -0300

    Desafio Banco Terminal concluído

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         //TODO: Conhecer e importar a classe Scanner
@@ -8,16 +10,31 @@ public class ContaTerminal {
 
         //Exibir a mensagem final da conta criada
 
-        int numero = 0001;
-        String agencia = "1234-5";
-        String nome_cliente = "";
-        float saldo = 0;
-
-        System.out.println("Por favor, digite o número da agência!");
+        Scanner scanner = new Scanner(System.in);
+      
 
 
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.nextLine();
 
-        System.out.println("Olá " + nome_cliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
+        System.out.println("Por favor, digite o número da Conta:");
+        int numero = scanner.nextInt();
+        scanner.nextLine();
 
+        System.out.println("Por favor, digite o nome do Cliente:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo:");
+        double saldo = scanner.nextDouble();
+
+
+
+        //System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
+        // Exibindo a mensagem de sucesso com saldo formatado
+        String mensagem = String.format("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo R$%.2f já está disponível para saque.",nomeCliente, agencia, numero, saldo);
+        System.out.println(mensagem);
+
+
+        scanner.close();
     }
 }
